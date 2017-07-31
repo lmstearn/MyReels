53652a4078d005ca70b5f45ab7ebe63f6bd2ac35
lmstearn <lmstearn@ozemail.com.au>
2017-08-01 00:50:01 +1000

    Add files via upload

---
+++ Help/NewAliases.h
@@ -0,0 +1,22 @@
+; Alias File written automatically by HHPMod Version 3.2.2
+; May 04, 2017, 04:14 PM
+; Automatic aliases for normal topics ;;;;;;;;;;;;;;;;;;;;;;;;;;
+_CH5TB = html\Free Games.htm
+_CH6TB = html\Free Spins (1).htm
+_CH7TB = html\Free Spins (2).htm
+_CH8TB = html\Edit Reels Configuration Screen.htm
+_CH9TB = html\Configure New Pictures Screen.htm
+_CH.TB = html\General.htm
+_CH_TB = html\Sounds.htm
+1LV5_X7 = html\GeneralPP.htm
+_CI5TB = html\Appearance.htm
+1LV6_X7 = html\Text.htm
+32C4B3Y = html\Game Screen.htm
+32C4C3Y = html\About Screen.htm
+32C4D3Y = html\Hall_of_Fame Screen.htm
+32C4E3Y = html\Device Info Screen.htm
+32C4F3Y = html\Profile Configuration Manager Screen.htm
+GCU7V7 = html\Configure Thumbnails Screen.htm
+GCV2V7 = html\Picture Info.htm
+GCV3V7 = html\Bitmap Finder Screen.htm
+GD29V7 = html\Welcome!.htm
---
+++ Help/NewMap.h
@@ -0,0 +1,23 @@
+; Map File written automatically by HHPMod Version 3.2.2
+; May 04, 2017, 04:14 PM
+;
+#define _CH5TB 9
+#define _CH6TB 10
+#define _CH7TB 11
+#define _CH8TB 6
+#define _CH9TB 7
+#define _CH.TB 12
+#define _CH_TB 13
+#define 1LV5_X7 8
+#define _CI5TB 15
+#define 1LV6_X7 14
+#define 21KSYK4 0
+#define 32C4B3Y 1
+#define 32C4C3Y 2
+#define 32C4D3Y 3
+#define 32C4E3Y 4
+#define 32C4F3Y 5
+#define GCU7V7 16
+#define GCV2V7 17
+#define GCV3V7 19
+#define GD29V7 18
